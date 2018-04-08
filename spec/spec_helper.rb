0a756d718bdc25b43387398827fc02bdd6ea9237
Ricardo Otero <oterosantos@gmail.com>
2018-04-08 19:02:42 +0100

    Rollback the use of __dir__ because of old versions of ruby

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-$LOAD_PATH.unshift File.expand_path('../lib', __dir__)
+$LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
 
 require 'cloudflare_localizable'
 require 'action_controller'
